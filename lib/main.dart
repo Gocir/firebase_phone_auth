d23b9da2c3d0f5e9f10a2145ce645cd0720d5eee
Gocir <ridwanwanwan72@gmail.com>
2020-10-02 20:16:18 +0700

    Memperbarui Data (Masih Error karena country code picker)

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
+import 'package:firebase_phone_auth/providers/phoneAuth.dart';
+import 'package:firebase_phone_auth/screens/login.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_phone_auth/screens/loginPage.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: LoginPage(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => PhoneAuthDataProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        home: PhoneAuthGetPhone(),
         debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
